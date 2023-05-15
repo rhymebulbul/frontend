67656e7a2661f57e82dc4e35d610e7a82bb333d2
weiliang chen <wche0041@student.monash.edu>
2023-05-15 23:50:26 +1000

    work on persona page

--- src/components/Page/Persona/PersonaPage.js
+++ src/components/Page/Persona/PersonaPage.js
@@ -9,21 +9,44 @@ const PersonaPage = (props) => {
             <Header />
             <div className={classes.main}>
                 <div className={classes.left}>
-                    <h3>left</h3>
+                    <div className={classes.photo}></div>
+                    <div className={classes.detail}>
+                        <div className={classes.line}><p>Name: name123</p></div>
+                        <div className={classes.line}><p>Age: 60</p></div>
+                        <div className={classes.line}><p>Job: Retiree</p></div>
+                    </div>
                 </div>
                 <div className={classes.vl}>
 
                 </div>
                 <div className={classes.right}>
-                    <h3>right</h3>
+                    <div className={classes.row}>
+                        <div className={classes.factor1}>
+                            <h4>Living Arrangement: </h4>
+                            <p>Lives all by himself after his wift passed away a year ago</p>
+                        </div>
+                        <div className={classes.factor2}>
+                            <h4>Frustration: </h4>
+                            <p>Lives all by himself after his wift passed away a year ago</p>
+                        </div>
+                    </div>
+                    <div className={classes.factor3}>
+                        <h4>Goal: </h4>
+                        <p>Lives all by himself after his wift passed away a year ago</p>
+                    </div>
+                    <div className={classes.factor4}>
+                        <h4> Adaptation to Technology: </h4>
+                        <p>Lives all by himself after his wift passed away a year ago</p>
+                    </div>
+                    <div className={classes.NextButton}>
+                        <NextButton name="EXPORT" />
+                        <NextButton name="REGENERATe" />
+                        <NextButton name="EDIT" />
+                        <NextButton name="SAVE" />
+                    </div>
                 </div>
             </div>
-            <div className={classes.NextButton}>
-                <NextButton name = "EXPORT"/>
-                <NextButton name = "REGENERATe"/>
-                <NextButton name = "EDIT"/>
-                <NextButton name = "SAVE"/>
-            </div>
+
         </Fragment>
     );
 };
